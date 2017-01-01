3d6ced3c4bfd21852339b6d26c79c643ca13faae
c9s <yoanlin93@gmail.com>
2017-01-02 02:43:53 +0800

    remove testing php info

--- ext/reducer/reducer.c
+++ ext/reducer/reducer.c
@@ -68,8 +68,8 @@ PHP_RSHUTDOWN_FUNCTION(reducer)
 PHP_MINFO_FUNCTION(reducer)
 {
     php_info_print_table_start();
-    php_info_print_table_row( 2, "Input Validation and Filtering", "enabled" );
-    php_info_print_table_row( 2, "Revision", "$Id: 9cc4bdc40f4013d538d685f912be2e9698c649cc $");
+    // php_info_print_table_row( 2, "Input Validation and Filtering", "enabled" );
+    // php_info_print_table_row( 2, "Revision", "$Id: 9cc4bdc40f4013d538d685f912be2e9698c649cc $");
     php_info_print_table_end();
     DISPLAY_INI_ENTRIES();
 }
